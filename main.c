80757966c240735b8f1e494ea393ba5c9ccac278
Martin Lund Haug <martlh@stud.ntnu.no>
2018-03-22 20:13:01 +0100

    added main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "elev.h"
+#include <stdio.h>
+#include "channels.h"
+#include "queue.h"
+#include "fsm.h"
+
+int main() {
+
+
+
+
+
+    // Initialize hardware
+    if (!elev_init()) {
+        printf("Unable to initialize elevator hardware!\n");
+        return 1;
+
+    }
+     printf("Press STOP button to stop elevator and exit program.\n");
+    while(1){
+ 
+      fsm();
+}
+
+
+}
